6386ce13c06730fe2ae4f7e5cc24184fcf50a415
tawandalloyd <51161179+tawandalloyd@users.noreply.github.com>
2023-08-29 14:56:17 +0200

    snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,3 +40,5 @@ streamlit.dataframe(my_data_rows)
 
 fruit_choices = streamlit.text_input('what fruit would you like to add?', 'jackfruit')
 streamlit.write('Thanks for adding', fruit_choices)
+
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
