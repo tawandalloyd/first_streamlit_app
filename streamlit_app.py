1fc4b392265b4737d253fde5cdd920b1f216b4e5
tawandalloyd <51161179+tawandalloyd@users.noreply.github.com>
2023-08-29 15:01:18 +0200

    stopping the streamlit code while we troubleshoot

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('My Parents healthy dinner')
 
@@ -30,6 +31,9 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
+
+#don't run anything while we troubleshoot
+streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
